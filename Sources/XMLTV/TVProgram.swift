e4c64e0312797bc73541e11bed1da1e214c4ed91
Conor Mongey <conor@mongey.net>
2020-06-13 22:11:04 +0100

    Add public initializers to TVChannel and TVProgram

--- Sources/XMLTV/TVProgram.swift
+++ Sources/XMLTV/TVProgram.swift
@@ -13,5 +13,30 @@ public struct TVProgram {
     public let episode: String?
     public let icon: String?
     public let rating: String?
-}
 
+    public init(start: Date?,
+                stop: Date?,
+                channel: TVChannel,
+                title: String?,
+                description: String?,
+                credits: [String: String],
+                date: String?,
+                categories: [String],
+                country: String?,
+                episode: String?,
+                icon: String?,
+                rating: String?) {
+        self.start = start
+        self.stop = stop
+        self.channel = channel
+        self.title = title
+        self.description = description
+        self.credits = credits
+        self.date = date
+        self.categories = categories
+        self.country = country
+        self.episode = episode
+        self.icon = icon
+        self.rating = rating
+    }
+}
